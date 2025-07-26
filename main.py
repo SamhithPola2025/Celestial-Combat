cd428bc7b81ca59db85a879b72b1b25add72d68a
Samhith Pola <Samhith.pola@gmail.com>
2025-07-27 00:13:56 +0200

    feat: add dragon city style fight window, polish fight logic, debug collision, improve UI and

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ camera.fov = 10
 menu_parent = Entity()
 settings_menu = Entity(enabled=False)
 grass_entities = []
+fight_window = Entity(parent=camera.ui, enabled=False)
+fight_text = Text('Battle!', parent=fight_window, y=0.2, scale=3, enabled=True)
+run_btn = Button(text='Skidaddle', parent=fight_window, y=-0.3, scale=(2,1), enabled=True)
+attack_button = Button(text='Attack', parent=fight_window, y=-0.1, scale=(2,1), enabled=True)
+player_health = 100
+enemy_health = 100
+fight_active = False
 
-wandering_trader_not_minecraft_stolen_dont_sue_me = Entity(model='quad', color=color.green, scale=(0.3, 0.5), position=(3,3,1), z=1, enabled=False)
-
-grassinstance = Entity(model='quad', z=1, color=color.green, scale=(0.05, 0.1), y=-0.25, enabled=False)
-player = Entity(model='quad', z=1, color=color.orange, scale=(0.3, 0.5), y=1, enabled=False)
+wandering_trader_not_minecraft_stolen_dont_sue_me = Entity(model='quad', color=color.green, scale=(0.3, 0.5), position=(3,3,1), z=1, enabled=True, collider='box')
+grassinstance = Entity(model='quad', z=1, color=color.green, scale=(0.05, 0.1), y=-0.25, enabled=True, collider='box')
+player = Entity(model='quad', z=1, color=color.orange, scale=(0.3, 0.5), y=1, enabled=True, collider='box')
 
 playerspeed = 5
 
@@ -30,6 +36,34 @@ music_files = [
     r'music/space-ranger-moire-main-version-03-04-10814.mp3',
     r'music/easy-arcade-hartzmann-main-version-28392-02-32.mp3'
 ]
+
+def start_fight():
+    global fight_active
+    fight_active = True
+    fight_window.enabled = True
+    player.enabled = False
+    fight_text.text = "Battle! Enemy HP: " + str(enemy_health)
+
+def end_fight():
+    global fight_active
+    fight_active = False
+    fight_window.enabled = False
+    player.enabled = True
+
+def attack():
+    global enemy_health
+    enemy_health -= 20
+    fight_text.text = f'Enemy health: {enemy_health}'
+    if enemy_health <= 0:
+        fight_text.text = "You Win!"
+        invoke(end_fight, delay=1)
+
+def run():
+    fight_text.text = "You ran away!"
+    invoke(end_fight, delay=1)
+
+attack_button.on_click = attack
+run_btn.on_click = run
 
 music_index = 0
 current_music = None
@@ -86,15 +120,19 @@ def tradercode(move_distance=3, speed=2):
     wandering_trader_not_minecraft_stolen_dont_sue_me.update = trader_update
 
 def start_game():
+    global enemy_health, player_health
     menu_parent.enabled = False
     player.enabled = True
     grassinstance.enabled = True
     camera.position = (player.x, player.y, -10)
-    background2.enabled=True
-    background.enabled=False
+    background2.enabled = True
+    background.enabled = False
     grassgen(150, 12)
-    wandering_trader_not_minecraft_stolen_dont_sue_me.enabled=True
+    wandering_trader_not_minecraft_stolen_dont_sue_me.enabled = True
     tradercode()
+    enemy_health = 100
+    player_health = 100
+    fight_window.enabled = False
 
 def quit_game():
     application.quit()
@@ -123,9 +161,9 @@ def LoadMainMenu():
 def LoadSettingsMenu():
     global volume_slider, current_music
 
-    Text("Settings", parent=settings_menu, scale=35, y=2,x = -2, z=0, font='Bitcountprop.ttf')
+    Text("Settings", parent=settings_menu, scale=35, y=2, x=-2, z=0, font='Bitcountprop.ttf')
 
-    volume_slider = Slider(scale=20, min=0, max=10, default=0.5, step=0.1, y=0.1, x = -5, parent=settings_menu)
+    volume_slider = Slider(scale=20, min=0, max=10, default=0.5, step=0.1, y=0.1, x=-5, parent=settings_menu)
 
     def on_volume_change():
         if current_music:
@@ -133,12 +171,21 @@ def LoadSettingsMenu():
 
     volume_slider.on_value_changed = on_volume_change
 
-    Text("Volume", parent=settings_menu, y=-0.5, scale=10, z=0, x=-0.5,font='Bitcountprop.ttf')
+    Text("Volume", parent=settings_menu, y=-0.5, scale=10, z=0, x=-0.5, font='Bitcountprop.ttf')
 
     back_btn = Button(text='Back', scale=(2.5, 1.5), y=-2, parent=settings_menu, on_click=back_to_menu)
     back_btn.text_entity.font = 'Bitcountprop.ttf'
 
 def update():
+
+    print("update")
+    print('player enabled:', player.enabled)
+    print('trader enabled:', wandering_trader_not_minecraft_stolen_dont_sue_me.enabled)
+
+    if player.enabled and wandering_trader_not_minecraft_stolen_dont_sue_me.enabled and not fight_active:
+        if player.intersects(wandering_trader_not_minecraft_stolen_dont_sue_me).hit:
+            start_fight()
+
     if player.enabled:
         x = held_keys['d'] - held_keys['a']
         y = held_keys['w'] - held_keys['s']
@@ -149,6 +196,7 @@ def update():
         for grass in grass_entities:
             if player.intersects(grass).hit:
                 speed *= 0.6
+                print("hit")
                 break
 
         if move.length() > 0:
@@ -162,7 +210,6 @@ def update():
 
 LoadMainMenu()
 LoadSettingsMenu()
-
 play_next_song()
 
 window.title = 'Celestial-Combat V1.1'
